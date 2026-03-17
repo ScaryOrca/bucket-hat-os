9c1928293a9cf77d6eca150a8a710802b7b1833b
Matthew Warner <mbw227@gmail.com>
2026-03-16 22:04:06 -0500

    fix: remove enable-kmscon.sh and use recipe.yml to enable kmscon

--- files/scripts/enable-kmscon.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Install kmscon.
-dnf install -y kmscon kmscon-pango kmscon-gl
-
-# Set as default VT.
-ln -s /usr/lib/systemd/system/kmsconvt@.service /usr/etc/systemd/system/autovt@.service
