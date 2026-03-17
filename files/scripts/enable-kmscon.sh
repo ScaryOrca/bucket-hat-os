e245ef4a58c9c8b1a50a1c19e6c93bfc6b99b3a4
Matthew Warner <mbw227@gmail.com>
2026-03-16 21:44:43 -0500

    fix: add -y

--- files/scripts/enable-kmscon.sh
+++ files/scripts/enable-kmscon.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 # Install kmscon.
-dnf install kmscon kmscon-pango kmscon-gl
+dnf install -y kmscon kmscon-pango kmscon-gl
 
 # Set as default VT.
 ln -s /usr/lib/systemd/system/kmsconvt@.service /usr/etc/systemd/system/autovt@.service
